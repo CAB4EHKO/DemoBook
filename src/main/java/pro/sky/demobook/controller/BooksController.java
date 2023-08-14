fc95d19b9b4d1cb4280d2c2695aed005d33b468f
Savchenko <savchenko.ra@yandex.ru>
2023-08-14 16:22:28 +0300

    Process

--- src/main/java/pro/sky/demobook/controller/BooksController.java
+++ src/main/java/pro/sky/demobook/controller/BooksController.java
@@ -1,12 +1,18 @@
 package pro.sky.demobook.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import pro.sky.demobook.model.Book;
+import pro.sky.demobook.model.BookCover;
+import pro.sky.demobook.service.BookCoverService;
 import pro.sky.demobook.service.BookService;
 
+import java.io.IOException;
 import java.util.Collection;
 
 @RestController
@@ -14,12 +20,13 @@ import java.util.Collection;
 public class BooksController {
 
 
-    public BooksController(BookService bookService) {
-        this.bookService = bookService;
-    }
-
     private final BookService bookService;
+    private final BookCoverService bookCoverService;
 
+    public BooksController(BookService bookService, BookCoverService bookCoverService) {
+        this.bookService = bookService;
+        this.bookCoverService = bookCoverService;
+    }
 
     @GetMapping("{id}")
     public ResponseEntity<Book> getBookInfo(@PathVariable Long id) {
@@ -46,9 +53,31 @@ public class BooksController {
         return ResponseEntity.ok(bookService.getAllBooks());
     }
 
+    @GetMapping(value = "/{id}/cover/preview")
+    public ResponseEntity<byte[]> downloadCover(@PathVariable Long id) {
+        BookCover bookCover = bookCoverService.findBookCover(id);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(bookCover.getMediaType()));
+        headers.setContentLength(bookCover.getPreview().length);
+
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(bookCover.getPreview());
+    }
+
+    @GetMapping
+
     @PostMapping
     public Book creatBook(Book book) {
         return bookService.creatBook(book);
+    }
+
+    @PostMapping(value = "/{id}/cover", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadCover(@PathVariable Long id, @RequestParam MultipartFile cover) throws IOException {
+        if (cover.getSize() >= 1024 * 300) {
+            ResponseEntity.badRequest().body("File is too big");
+        }
+        bookCoverService.uploadCover(id, cover);
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping
